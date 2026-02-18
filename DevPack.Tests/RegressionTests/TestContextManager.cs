6d63ffa9e0fd801e6c332e1b782c3c7767a182ea
SKYLINE2\ThomasRE <thomas.remmery@skyline.be>
2026-02-18 17:15:26 +0100

    Attempt to fix sonar remark again

--- DevPack.Tests/RegressionTests/TestContextManager.cs
+++ DevPack.Tests/RegressionTests/TestContextManager.cs
@@ -1,5 +1,6 @@
 ﻿namespace RT_PeopleAndOrganizations.RegressionTests
 {
+	[TestClass]
 	public class TestContextManager
 	{
 		public static IntegrationTestContext SharedTestContext { get; } = new IntegrationTestContext();
