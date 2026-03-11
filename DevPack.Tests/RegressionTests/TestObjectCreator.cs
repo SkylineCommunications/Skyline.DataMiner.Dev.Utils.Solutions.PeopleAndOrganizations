20fb1f9ba7b5d07ea72770d5d4a4d17a5de3ecfd
Thomas <thomas.remmery@skyline.be>
2026-03-11 15:46:12 +0100

    Fix some more sonar remarks.

--- DevPack.Tests/RegressionTests/TestObjectCreator.cs
+++ DevPack.Tests/RegressionTests/TestObjectCreator.cs
@@ -84,53 +84,6 @@
 			}
 		}
 
-		private void PeopleCleanup()
-		{
-		}
-
-		private void TeamsCleanup()
-		{
-		}
-
-		private void OrganizationsCleanup()
-		{
-			var organizations = Api.Organizations.Read(createdOrganizationIds.ToArray());
-
-			try
-			{
-				var toDeprecate = organizations.Where(x => x.State == OrganizationState.Active);
-
-				Api.Organizations.Deprecate(toDeprecate);
-			}
-			catch
-			{
-				// Ignore cleanup errors
-			}
-
-			Api.Organizations.Delete(organizations.ToArray());
-		}
-
-		private void ExperienceCleanup()
-		{
-			var experience = Api.Experience.Read(createdExperienceIds.ToArray());
-
-			Api.Experience.Delete(experience.ToArray());
-		}
-
-		private void CategoriesCleanup()
-		{
-			var categories = Api.Categories.Read(createdCategoryIds.ToArray());
-
-			Api.Categories.Delete(categories.ToArray());
-		}
-
-		private void RolesCleanup()
-		{
-			var roles = Api.Roles.Read(createdRoleIds.ToArray());
-
-			Api.Roles.Delete(roles.ToArray());
-		}
-
 		public Organization CreateOrganization(Organization organization)
 		{
 			var createdOrganization = Api.Organizations.Create(organization);
@@ -253,6 +206,53 @@
 
 				throw;
 			}
+		}
+
+		private void PeopleCleanup()
+		{
+		}
+
+		private void TeamsCleanup()
+		{
+		}
+
+		private void OrganizationsCleanup()
+		{
+			var organizations = Api.Organizations.Read(createdOrganizationIds.ToArray());
+
+			try
+			{
+				var toDeprecate = organizations.Where(x => x.State == OrganizationState.Active);
+
+				Api.Organizations.Deprecate(toDeprecate);
+			}
+			catch
+			{
+				// Ignore cleanup errors
+			}
+
+			Api.Organizations.Delete(organizations.ToArray());
+		}
+
+		private void ExperienceCleanup()
+		{
+			var experience = Api.Experience.Read(createdExperienceIds.ToArray());
+
+			Api.Experience.Delete(experience.ToArray());
+		}
+
+		private void CategoriesCleanup()
+		{
+			var categories = Api.Categories.Read(createdCategoryIds.ToArray());
+
+			Api.Categories.Delete(categories.ToArray());
+		}
+
+		private void RolesCleanup()
+		{
+			var roles = Api.Roles.Read(createdRoleIds.ToArray());
+
+			Api.Roles.Delete(roles.ToArray());
 		}
 	}
 }
