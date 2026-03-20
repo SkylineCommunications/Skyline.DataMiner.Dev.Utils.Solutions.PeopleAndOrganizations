1372991a20b42b1db8c7c6e3f5ae8b593e2f6b10
Jens Vandewalle <jens.vandewalle@skyline.be>
2026-03-20 13:39:31 +0100

    Add tests for invalid ExperienceId/OrganizationId on Person
    Added ExperienceAssignmentTests and OrganizationAssignmentTests to verify exceptions are thrown when assigning non-existent ExperienceId or OrganizationId to a Person, both on creation and update. Cleaned up unused usings and reformatted namespace in CategoryAssignmentTests.cs.

---
+++ DevPack.Tests/PeopleOrganization/People/OrganizationAssignmentTests.cs
@@ -0,0 +1,100 @@
+﻿namespace RT_PeopleAndOrganizations.PeopleOrganization.People
+{
+	using System;
+	using System.Linq;
+
+	using RT_PeopleAndOrganizations.RegressionTests;
+
+	using Skyline.DataMiner.Solutions.PeopleAndOrganizations.API;
+	using Skyline.DataMiner.Solutions.PeopleAndOrganizations.Exceptions;
+
+	[TestClass]
+	[TestCategory("IntegrationTest")]
+	public sealed class OrganizationAssignmentTests : IDisposable
+	{
+		private readonly TestObjectCreator objectCreator;
+
+		public OrganizationAssignmentTests()
+		{
+			objectCreator = new TestObjectCreator(TestContext);
+		}
+
+		private static IntegrationTestContext TestContext => TestContextManager.SharedTestContext;
+
+		public void Dispose()
+		{
+			objectCreator.Dispose();
+		}
+
+		[TestMethod]
+		public void CreateWithNotExistingThrowsException()
+		{
+			var prefix = Guid.NewGuid();
+			var organizationId = Guid.NewGuid();
+
+			var person = new Person
+			{
+				Name = $"{prefix}_Person",
+				OrganizationId = organizationId,
+			};
+
+			PeopleAndOrganizationsException? expectedException = null;
+			try
+			{
+				person = objectCreator.CreatePerson(person);
+			}
+			catch (PeopleAndOrganizationsException ex)
+			{
+				expectedException = ex;
+			}
+
+			Assert.IsNotNull(expectedException, "Expected exception was not thrown.");
+
+			Assert.AreEqual(1, expectedException.TraceData.ErrorData.Count);
+			var personError = expectedException.TraceData.ErrorData.OfType<PersonError>().SingleOrDefault();
+			Assert.IsNotNull(personError);
+
+			var personOrganizationNotFoundError = personError as PersonOrganizationNotFoundError;
+			Assert.IsNotNull(personOrganizationNotFoundError);
+			Assert.AreEqual($"Organization with ID '{organizationId}' not found.", personOrganizationNotFoundError.ErrorMessage);
+			Assert.AreEqual(person.Id, personOrganizationNotFoundError.Id);
+			Assert.AreEqual(organizationId, personOrganizationNotFoundError.OrganizationId);
+		}
+
+		[TestMethod]
+		public void UpdateWithNotExistingThrowsException()
+		{
+			var prefix = Guid.NewGuid();
+			var organizationId = Guid.NewGuid();
+
+			var person = new Person
+			{
+				Name = $"{prefix}_Person",
+			};
+			person = objectCreator.CreatePerson(person);
+
+			PeopleAndOrganizationsException? expectedException = null;
+			try
+			{
+				person.OrganizationId = organizationId;
+				TestContext.Api.People.Update(person);
+			}
+			catch (PeopleAndOrganizationsException ex)
+			{
+				expectedException = ex;
+			}
+
+			Assert.IsNotNull(expectedException, "Expected exception was not thrown.");
+
+			Assert.AreEqual(1, expectedException.TraceData.ErrorData.Count);
+			var personError = expectedException.TraceData.ErrorData.OfType<PersonError>().SingleOrDefault();
+			Assert.IsNotNull(personError);
+
+			var personOrganizationNotFoundError = personError as PersonOrganizationNotFoundError;
+			Assert.IsNotNull(personOrganizationNotFoundError);
+			Assert.AreEqual($"Organization with ID '{organizationId}' not found.", personOrganizationNotFoundError.ErrorMessage);
+			Assert.AreEqual(person.Id, personOrganizationNotFoundError.Id);
+			Assert.AreEqual(organizationId, personOrganizationNotFoundError.OrganizationId);
+		}
+	}
+}
