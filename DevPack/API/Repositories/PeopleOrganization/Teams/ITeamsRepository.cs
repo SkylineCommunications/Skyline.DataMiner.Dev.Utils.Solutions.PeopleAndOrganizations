4da3bf7634a1fe8b7f592cbc0530711d39788961
Jens Vandewalle <jens.vandewalle@skyline.be>
2026-04-07 15:29:01 +0200

    Update XML docs for Deprecate/MakeBookable params
    Clarify in ITeamsRepository that the 'teams' parameter for Deprecate and MakeBookable methods must not be null, removing the previous "or empty" restriction from the documentation. No functional code changes were made.

--- DevPack/API/Repositories/PeopleOrganization/Teams/ITeamsRepository.cs
+++ DevPack/API/Repositories/PeopleOrganization/Teams/ITeamsRepository.cs
@@ -53,7 +53,7 @@
 		/// <summary>
 		/// Marks the specified teams as deprecated, indicating that they are no longer recommended for use.
 		/// </summary>
-		/// <param name="teams">A collection of teams to be marked as deprecated. Cannot be null or empty.</param>
+		/// <param name="teams">A collection of teams to be marked as deprecated. Cannot be null.</param>
 		/// <returns>A read-only collection of deprecated teams.</returns>
 		IReadOnlyCollection<Team> Deprecate(IEnumerable<Team> teams);
 
@@ -81,7 +81,7 @@
 		/// <summary>
 		/// Marks the specified teams as bookable, indicating that they can be reserved or scheduled.
 		/// </summary>
-		/// <param name="teams">A collection of teams to be marked as bookable. Cannot be null or empty.</param>
+		/// <param name="teams">A collection of teams to be marked as bookable. Cannot be null.</param>
 		/// <returns>A read-only collection of bookable teams.</returns>
 		IReadOnlyCollection<Team> MakeBookable(IEnumerable<Team> teams);
 
